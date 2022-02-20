b12e8a50e21ff168044dd624ad1c3e9029a4c84b
Darryl Brooks <darrylbrooks13@gmail.com>
2022-02-19 22:58:51 -0600

    chore: making icons clickable

--- lib/widgets/footer.dart
+++ lib/widgets/footer.dart
@@ -1,57 +1,56 @@
 import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Footer extends StatelessWidget {
   const Footer({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return 
-      Padding(
+    return Padding(
         padding: const EdgeInsets.all(20),
-        child: 
-          Column( 
-          children:[
-            // Line
-            const SizedBox(
-              width: 1023,
-              child: Padding(
-                padding: EdgeInsets.all(20), 
-                child:Divider(
-                height: 0,
-                thickness:2,
-                color: Colors.black
-                )
-              ),
-            ),
-            SizedBox(
+        child: Column(children: [
+          // Line
+          const SizedBox(
+            width: 1023,
+            child: Padding(
+                padding: EdgeInsets.all(20),
+                child: Divider(height: 0, thickness: 2, color: Colors.black)),
+          ),
+          SizedBox(
               width: 280,
               child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                IconButton(
-                    iconSize: 48,
-                    onPressed: () {
-                      print('Twitter');
-                    },
-                    icon: const Icon(MdiIcons.twitter)),
-                IconButton(
-                    iconSize: 48,
-                    onPressed: () {
-                      print('Instagram');
-                    },
-                    icon: const Icon(MdiIcons.instagram)),
-                IconButton(
-                    iconSize: 48,
-                    onPressed: () {
-                      print('LinkedIn');
-                    },
-                    icon: const Icon(MdiIcons.linkedin)),
-              ],
-            )
-            )
-          ]
-          )
-      );
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  IconButton(
+                      iconSize: 48,
+                      onPressed: () async{
+                        if (await canLaunch(
+                            'https://www.twitter.com/twitter/')) {
+                          await launch('https://www.twitter.com/twitter/');
+                        }
+                      },
+                      icon: const Icon(MdiIcons.twitter)),
+                  IconButton(
+                      iconSize: 48,
+                      onPressed: () async {
+                        if (await canLaunch(
+                            'https://www.instagram.com/Apxflex')) {
+                          await launch('https://www.instagram.com/instagram/');
+                        }
+                      },
+                      icon: const Icon(MdiIcons.instagram)),
+                  IconButton(
+                      iconSize: 48,
+                      onPressed: () async {
+                        if (await canLaunch(
+                            'https://www.linkedin.com/in/linkedin/')) {
+                          await launch('https://www.linkedin.com/in/linkedin/');
+                        }
+                      },
+                      icon: const Icon(MdiIcons.linkedin)),
+                ],
+              ))
+        ]));
   }
 }
